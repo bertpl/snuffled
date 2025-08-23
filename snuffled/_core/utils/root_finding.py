3390845977b39acda5fcbef65ef147a2f66cdf6b
Bert Pluymers <bert.pluymers@gmail.com>
2025-08-23 12:09:10 +0200

    Extend root finding utils to also determine root 'width'.

--- snuffled/_core/utils/root_finding.py
+++ snuffled/_core/utils/root_finding.py
@@ -1,17 +1,46 @@
 from typing import Callable
 
-from .constants import EPS
-
-# smallest interval we will consider for root-finding.
-_DX_MIN = EPS * EPS
+import numpy as np
 
 
-def find_root(fun: Callable[[float], float], x_min: float, x_max: float):
+def find_root_and_width(
+    fun: Callable[[float], float], x_min: float, x_max: float, dx_min: float
+) -> tuple[float, float]:
     """
-    Find root in given interval with maximal precision up to the limit of absolute interval width _DX_MIN.
-    The returned root x will either...
-      - have f(x)==0.0 exactly
-      - be the midpoint the smallest bracketing interval that shows a sign change.
+    Combines find_root() and determine_root_width(), such that in all cases we determine the root 'range'
+    as [root_min, root_max] in a reliable way.
+
+    :param fun: Function for which we want to find a root
+    :param x_min: (float) left edge of interval [x_min, x_max] in which to search for root
+    :param x_max: (float) right edge of interval [x_min, x_max] in which to search for root
+    :param dx_min: (float > 0) smallest interval to consider when looking for a root or determining its width
+    :return: (root_min, root_max)-tuple satisfying the following invariants:
+                A) x_min <= root_min <= root_max <= x_max
+                B) either of the following
+                    - fun(root_min) == fun(root_max) == 0.0    in which case [root_min, root_max] is the largest interval for which this holds
+                         OR
+                    - fun(root_min) * fun(root_max) < 0.0  and [root_min, root_max] is the smallest possible interval >dx_min for which this holds
+
+                    In both cases root_max-root_min gives an accurate idea of how well-defined (sharp vs wide) the root is.
+    """
+    root_min, root_max = find_root(fun, x_min, x_max, dx_min)
+    if root_min == root_max:
+        # this means fun(root_min)==fun(root_max)==0.0
+        return determine_root_width(fun, root_min, x_min, x_max, dx_min)
+    else:
+        # this means fun(root_min) * fun(root_max) < 0.0
+        return root_min, root_max
+
+
+def find_root(fun: Callable[[float], float], x_min: float, x_max: float, dx_min: float) -> tuple[float, float]:
+    """
+    Find root in given interval with maximal precision up to the limit of absolute interval width 'dx_min'.
+    The result is returned as an interval [root_min, root_max], which is always a sub-interval of [x_min, x_max].
+
+    Either of the following conditions holds:
+      - root_min == root_max  AND   fun(root_min) == fun(root_max) == 0.0
+            OR
+      - root_min < root_max   AND   fun(root_min) * fun(root_max) < 0.0
     """
 
     # --- sanity checks & initialization ------------------
@@ -21,12 +50,12 @@ def find_root(fun: Callable[[float], float], x_min: float, x_max: float):
     if fx_min * fx_max > 0:
         raise ValueError(f"No sign change in interval [{x_min}, {x_max}]; f(x_min)*f(x_max) = {fx_min * fx_max}")
     elif fx_min == 0.0:
-        return x_min
+        return x_min, x_min
     elif fx_max == 0.0:
-        return x_max
+        return x_max, x_max
 
     # --- bisection ---------------------------------------
-    while (x_max - x_min) > _DX_MIN:
+    while (x_max - x_min) > dx_min:
         # sample mid-point
         x_mid = 0.5 * (x_max + x_min)
         fx_mid = fun(x_mid)
@@ -34,10 +63,10 @@ def find_root(fun: Callable[[float], float], x_min: float, x_max: float):
         # decide how to go forward
         if fx_mid == 0.0:
             # found the exact root
-            return x_mid
+            return x_mid, x_mid
         elif (x_mid == x_min) or (x_mid == x_max):
             # fx_min & fx_max are so close to each other that fx_mid coincides with either due to rounding
-            return x_mid
+            return x_min, x_max
         elif fx_mid * fx_min > 0:
             x_min = x_mid
             fx_min = fx_mid
@@ -46,4 +75,65 @@ def find_root(fun: Callable[[float], float], x_min: float, x_max: float):
             fx_max = fx_mid
 
     # return mid-point if we haven't returned prematurely yet
-    return 0.5 * (x_max + x_min)
+    return x_min, x_max
+
+
+def determine_root_width(
+    fun: Callable[[float], float], root: float, x_min: float, x_max: float, dx_min: float
+) -> tuple[float, float]:
+    """
+    Determine the 'width' of a function root.  This function is only used in case find_root finds a root with
+    fun(root)==0.0.  In this case we try to find the largest interval around root (starting from interval size dx_min)
+    for which fun(x)==0.0.
+
+    This is a useful property to detect, since this determines how 'well-defined' the roots of the function are.
+    If they are considered 'wide' (i.e. order of magnitude close to the root-finding abs tolerance) they can start to
+    affect root-finding efficiency (in a way that might benefit some algorithms).
+
+    This function never calls 'fun' with arguments outside [x_min, x_max], which also inevitably limits the width
+    detection of roots very close to these interval edges.
+
+    NOTE: in case 'find_root' finds a root where root_min < root_max and hence fun(root_min) * fun(root_max) < 0,
+          it means it couldn't find an exact root within the smallest achievable interval size.
+          In other words [root_min, root_max] already represents the smallest possible interval size enclosing the root.
+          This could either be due to the fact that numerical rounding artifacts cause the function to jump from <0 to
+          >0 with a minimal change in x (as allowed by the float64 format), or because the function fundamentally
+          makes a jump around the 'root' (e.g. step(x)-0.5).
+    """
+
+    # --- init --------------------------------------------
+    dx_start = max(
+        dx_min / 2.0,  # taking a step of (dx_min/2) in two directions creates an interval of size dx_min
+        min(
+            np.nextafter(root, np.inf) - root,  # smallest step in negative direction
+            root - np.nextafter(root, -np.inf),  # smallest step in positive direction
+        ),
+    )
+    root_min = root  # left-most edge of interval where fun(x)==0
+    root_max = root  # right-most edge of interval where fun(x)==0
+
+    # --- search in + direction ---------------------------
+    dx = dx_start
+    while root_max < x_max:
+        x_cand = min(x_max, root + dx)
+        if fun(x_cand) != 0.0:
+            # we reached the edge of the root
+            break
+        else:
+            # fun(x) is still 0.0, so we continue increasing dx
+            root_max = x_cand
+            dx += max(dx_start, 0.1 * dx)  # increment dx in steps of 10% or dx_start (whichever is largest)
+
+    # --- search in - direction ----------------------------
+    dx = dx_start
+    while root_min > x_min:
+        x_cand = max(x_min, root - dx)
+        if fun(x_cand) != 0.0:
+            # we reached the edge of the root
+            break
+        else:
+            root_min = x_cand
+            dx += max(dx_start, 0.1 * dx)  # increment dx in steps of 10% or dx_start (whichever is largest)
+
+    # we're done
+    return root_min, root_max
--- tests/utils/test_root_finding.py
+++ tests/utils/test_root_finding.py
@@ -5,7 +5,7 @@ from typing import Callable
 import pytest
 
 from snuffled._core.utils.constants import EPS
-from snuffled._core.utils.root_finding import find_root
+from snuffled._core.utils.root_finding import determine_root_width, find_root, find_root_and_width
 
 
 # =================================================================================================
@@ -15,19 +15,26 @@ def f_linear(x: float, root: float) -> float:
     return x - root
 
 
-def f_quadratic(x: float, root: float):
+def f_quadratic(x: float, root: float) -> float:
     return (x - root) * (x + 11)
 
 
-def f_exp(x: float, root: float):
+def f_exp(x: float, root: float) -> float:
     return math.exp(x) - math.exp(root)
 
 
-def f_step(x: float, root: float):
+def f_step(x: float, root: float) -> float:
     if x >= root:
         return 1.0
     else:
         return -1.0
+
+
+def f_lin_with_wide_zero(x: float, root: float, zero_width: float) -> float:
+    if abs(x - root) <= (zero_width / 2):
+        return 0.0
+    else:
+        return x - root
 
 
 # =================================================================================================
@@ -42,47 +49,41 @@ def f_step(x: float, root: float):
     ],
 )
 @pytest.mark.parametrize(
-    "fun, x_expected, x_abs_tol",
+    "fun, dx_min, x_expected, x_abs_tol",
     [
-        (partial(f_linear, root=0.5), 0.5, EPS),
-        (partial(f_linear, root=-0.01), -0.01, 0.01 * EPS),
-        (partial(f_linear, root=1e-10), 1e-10, 1e-10 * EPS),
-        (partial(f_linear, root=1e-50), 1e-50, EPS * EPS),
-        (partial(f_quadratic, root=0.4), 0.4, EPS),
-        (partial(f_exp, root=0.6), 0.6, EPS),
-        (partial(f_exp, root=-0.9), -0.9, EPS),
-        (partial(f_step, root=-0.8), -0.8, EPS),
-        (partial(f_step, root=0.1), 0.1, EPS),
-        (partial(f_step, root=0.99), 0.99, EPS),
+        (partial(f_linear, root=0.5), EPS * EPS, 0.5, EPS),
+        (partial(f_linear, root=-0.01), EPS * EPS, -0.01, 0.01 * EPS),
+        (partial(f_linear, root=1e-10), EPS * EPS, 1e-10, 1e-10 * EPS),
+        (partial(f_linear, root=1e-50), EPS * EPS, 1e-50, EPS * EPS),
+        (partial(f_quadratic, root=0.4), EPS * EPS, 0.4, EPS),
+        (partial(f_exp, root=0.6), EPS * EPS, 0.6, EPS),
+        (partial(f_exp, root=-0.9), EPS * EPS, -0.9, EPS),
+        (partial(f_step, root=-0.8), EPS * EPS, -0.8, EPS),
+        (partial(f_step, root=0.1), EPS * EPS, 0.1, EPS),
+        (partial(f_step, root=0.99), EPS * EPS, 0.99, EPS),
+        (partial(f_step, root=0.0), EPS * EPS, 0.0, EPS * EPS),  # check if dx_min is handled correctly
+        (partial(f_step, root=0.0), EPS, 0.0, EPS),  # check if dx_min is handled correctly
+        (partial(f_linear, root=-1.0), EPS * EPS, -1.0, EPS),  # root at the edge if x_min=-1
+        (partial(f_linear, root=1.0), EPS * EPS, 1.0, EPS),  # root at the edge if x_max=1
     ],
 )
-def test_find_root(fun: Callable[[float], float], x_min: float, x_max: float, x_expected: float, x_abs_tol: float):
+def test_find_root(
+    fun: Callable[[float], float], x_min: float, x_max: float, dx_min: float, x_expected: float, x_abs_tol: float
+):
     # --- arrange -----------------------------------------
     x_expected_min = x_expected - x_abs_tol
     x_expected_max = x_expected + x_abs_tol
 
     # --- act ---------------------------------------------
-    x = find_root(fun, x_min, x_max)
+    root_min, root_max = find_root(fun, x_min, x_max, dx_min)
 
     # --- assert ------------------------------------------
-    assert x_expected_min <= x <= x_expected_max
-
-
-@pytest.mark.parametrize(
-    "fun, x_min, x_max, x_expected",
-    [
-        (partial(f_linear, root=-1.0), -1.0, 1.0, -1.0),
-        (partial(f_linear, root=1.0), -1.0, 1.0, 1.0),
-        (partial(f_exp, root=-2.0), -2.0, 1.0, -2.0),
-        (partial(f_exp, root=3.0), -1.0, 3.0, 3.0),
-    ],
-)
-def test_find_root_edge_cases(fun: Callable[[float], float], x_min: float, x_max: float, x_expected: float):
-    # --- act ---------------------------------------------
-    x = find_root(fun, x_min, x_max)
-
-    # --- assert ------------------------------------------
-    assert x == x_expected
+    assert x_expected_min <= root_min <= root_max <= x_expected_max
+    assert (root_min < root_max) or (fun(root_min) == fun(root_min) == 0.0)
+    if root_min < root_max:
+        # this means we had to stop due to floating point rounding or dx_min
+        # so let's check we didn't overdo it and make it _too_ accurate (which would waste function evaluations)
+        assert (root_max - root_min) > (dx_min / 2)
 
 
 @pytest.mark.parametrize(
@@ -98,4 +99,50 @@ def test_find_root_value_error(x_min: float, x_max: float):
 
     # --- act & assert ------------------------------------
     with pytest.raises(ValueError):
-        _ = find_root(fun, x_min, x_max)
+        _ = find_root(fun, x_min, x_max, dx_min=EPS)
+
+
+@pytest.mark.parametrize("true_root", [-0.9, -0.1, 1 / math.pi, 0.6789])
+@pytest.mark.parametrize("true_root_width", [1e-15, 1e-10, 1e-5, 1e-3])
+def test_determine_root_width(true_root: float, true_root_width: float):
+    # --- arrange -----------------------------------------
+    fun = partial(f_lin_with_wide_zero, root=true_root, zero_width=true_root_width)
+    x_min = -1.0
+    x_max = 1.0
+
+    # --- act ---------------------------------------------
+    root_min, root_max = find_root_and_width(fun, x_min, x_max, dx_min=EPS * EPS)
+
+    # --- assert ------------------------------------------
+    assert root_min <= true_root <= root_max
+    assert 0.8 * true_root_width <= (root_max - root_min) <= 1.2 * true_root_width
+
+
+@pytest.mark.parametrize(
+    "true_root, true_root_width",
+    [
+        (-0.8, 0.5),
+        (0.7, 0.8),
+        (-0.99, 0.1),
+        (0.93, 0.15),
+        (0.123, 3.1),
+    ],
+)
+def test_determine_root_width_edge_cases(true_root: float, true_root_width: float):
+    """This test set up such that each case has a zero so wide it crosses at least one edge of [x_min, x_max]"""
+
+    # --- arrange -----------------------------------------
+    fun = partial(f_lin_with_wide_zero, root=true_root, zero_width=true_root_width)
+    x_min = -1.0
+    x_max = 1.0
+
+    # --- act ---------------------------------------------
+    root_min, root_max = find_root_and_width(fun, x_min, x_max, dx_min=EPS * EPS)
+
+    # --- assert ------------------------------------------
+    assert root_min < root_max
+    assert root_min <= true_root <= root_max
+    assert root_max - root_min < true_root_width
+    assert x_min <= root_min <= x_max
+    assert x_min <= root_max <= x_max
+    assert (root_min == x_min) or (root_max == x_max)
