6b6a802577cce657f54c6de6076f4cc6ab68cbac
Bert Pluymers <bert.pluymers@gmail.com>
2025-08-26 16:09:48 +0200

    Add structured, randomized x_delta sampling method.

--- snuffled/_core/analysis/roots/single_root/helpers_curve_fitting.py
+++ snuffled/_core/analysis/roots/single_root/helpers_curve_fitting.py
@@ -1,9 +1,72 @@
+import math
 import random
 
 import numba
 import numpy as np
 from numba.core import types
 from numba.typed.typeddict import Dict
+
+
+# =================================================================================================
+#
+# =================================================================================================
+@numba.njit
+def compute_x_deltas(dx: float, k: int, seed: int = 42) -> np.ndarray:
+    """
+    Computes x_delta-array, containing values that can be used to sample function values around
+    a root:    f(root ± x_delta).
+
+    x_delta values are constructed such that...
+        - we will have 3*(1 + 2*k) values
+             - 1 group around   dx
+             - 1 group around 2*dx
+             - 1 group around 4*dx
+        - with each group consisting of...
+             - 1 sample at the reference point
+             - k samples below the reference point
+             - k samples symmetrically (geomean) above the reference point
+        - each of the 3 groups of samples will span a 2x range from 1/sqrt(2) -> sqrt(2) relative around the ref. value
+           - hence different groups do not overlap
+           - but they span a consecutive 8x range [dx/sqrt(2), 4*dx*sqrt(2)]
+
+    This will result in delta_x values with the following properties:
+        - median(x_deltas < dx*sqrt(2))    =  geomean(x_deltas < dx*sqrt(2))    =    dx
+        - median(x_deltas)                 =  geomean(x_deltas)                 =  2*dx
+        - median(x_deltas > 2*sqrt(2)*dx)  =  geomean(x_deltas > 2*sqrt(2)*dx)  =  4*dx
+
+    :param dx: (float) reference distance dx > 0
+    :param k: (int) sampling count parameter
+    :param seed: (int) seed for random number generator
+    :return: np.ndarray with 3*(1+2k) x_delta values, sorted in increasing order
+    """
+
+    # initialize
+    random.seed(seed)
+    x_deltas = np.zeros(3 + 6 * k)
+    x_deltas[0] = dx
+    x_deltas[1] = 2 * dx
+    x_deltas[2] = 4 * dx
+
+    # 6*k additional randomized samples
+    for j in range(k):
+        # set up iteration j
+        r_outer = 1 + ((math.sqrt(2) - 1) * random.random())
+        r_inner = 1 + ((math.sqrt(2) - 1) * random.random())
+        i_start = 3 + (6 * j)
+
+        # two values geo-symmetrically around dx
+        x_deltas[i_start] = dx * r_outer
+        x_deltas[i_start + 1] = dx / r_outer
+
+        # two values geo-symmetrically around 2*dx
+        x_deltas[i_start + 2] = 2 * dx * r_inner
+        x_deltas[i_start + 3] = 2 * dx / r_inner
+
+        # two values geo-symmetrically around 4*dx
+        x_deltas[i_start + 4] = 4 * dx * r_outer
+        x_deltas[i_start + 5] = 4 * dx / r_outer
+
+    return np.sort(x_deltas)
 
 
 # =================================================================================================
