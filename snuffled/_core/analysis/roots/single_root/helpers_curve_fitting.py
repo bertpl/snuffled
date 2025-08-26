41238aa2c6f9856ba7d56d8070744478c6efa09b
Bert Pluymers <bert.pluymers@gmail.com>
2025-08-26 14:04:38 +0200

    Rename parameters to a,b,c and add scaling factor a as an explicit parameter.

--- snuffled/_core/analysis/roots/single_root/helpers_curve_fitting.py
+++ snuffled/_core/analysis/roots/single_root/helpers_curve_fitting.py
@@ -1,5 +1,9 @@
+import random
+
 import numba
 import numpy as np
+from numba.core import types
+from numba.typed.typeddict import Dict
 
 
 # =================================================================================================
@@ -9,65 +13,190 @@ import numpy as np
 def fit_curve_brute_force(
     x: np.ndarray,
     fx: np.ndarray,
-    range_c_exp: tuple[float, float],
-    range_c_step: tuple[float, float],
-    exp_sign: float,
+    range_b: tuple[float, float],
+    range_c: tuple[float, float],
+    c_sign: float,
     n_grid: int,
-    c_reg: float,
+    reg: float,
     tol_c0: float,
     tol_c1: float,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Fits a curve of the following form to a list of (x, fx)-tuples:
 
-        f(x) = c_step + (1-c_step)*(x**c_exp)
+        g(x) = a * (b + (1-b)*(x^c))
 
-    by minimizing the L1 cost (mean of absolute deviations) over a rectangular grid of (c_exp, c_step)-values.
-    The grid is (n_grid x n_grid) values and is linear in c_step and logarithmic in c_exp.
+    by minimizing the L1 cost (mean of absolute deviations) over (a,b,c) search space:
+      - first we determine an appropriate value for 'a'
+      - then we optimize by brute force grid search over a rectangular grid of (b, c)-values.
+    The grid is (n_grid x n_grid) values and is linear in b and logarithmic in c.
 
-    This function does not return a single optimal (c_exp, c_step)-value, but returns all tuples in the search grid
+    This function does not return a single optimal (a, b, c)-value, but returns all tuples in the search grid
     whose cost is 'close enough' (as determined by tol_c0, tol_c1) to the optimal cost.
+
+    NOTE:
+      - we assume median(x) == 1
 
     :param x: (n,)-sized numpy array containing x values, with each x>0
     :param fx: (n,)-sized numpy array containing corresponding f(x) values
-    :param range_c_exp:  (c_exp_min, c_exp_max)    range of c_exp values  (>0)
-    :param range_c_step: (c_step_min, c_step_max)  range of c_step values
-    :param exp_sign: (float) sign of c_exp, i.e. if exp_sign=-1, we will search over range [-c_exp_max, -c_exp_min].
+    :param range_c:  (c_min, c_max)    range of c values  (>0)
+    :param range_b: (b_min, b_max)  range of b values
+    :param c_sign: (float) sign of c, i.e. if _sign=-1, we will search over range [-c_max, -c_min].
     :param n_grid: (int) number of grid values along each dimension
-    :param c_reg: (float) regularization coefficient that helps favour c_exp=1.0, c_step=0.0  (e.g. 1e-3)
+    :param reg: (float) regularization coefficient that helps favour c=1.0, b=0.0  (e.g. 1e-3)
     :param tol_c0: (float) Only those values whose cost <= cost_threshold are returned, with
     :param tol_c1: (float)   cost_threshold = tol_c0 + tol_c1*min(cost)
-    :return: (c_exp_values, c_step_values, cost_values)-tuples, each of which is a (k,)-sized numpy array with k>=1
-               i-th elements of these arrays should be interpreted as tuples (c_exp[i], c_step[i]) having cost[i]
+    :return: (a_values, b_values, c_values, cost_values)-tuples, each of which is a (k,)-sized numpy array with k>=1
+               i-th elements of these arrays should be interpreted as tuples (a[i], b[i], c[i]) having cost[i]
     """
 
-    # --- init --------------------------------------------
-    c_exp_min, c_exp_max = range_c_exp
-    c_step_min, c_step_max = range_c_step
+    # --- determine a -------------------------------------
+    a = float(np.median(fx))  # should be a reasonable value for 'a' given that median(x) == 1.0 and g(1)==a
 
-    c_exp_values = exp_sign * np.exp(np.linspace(np.log(c_exp_min), np.log(c_exp_max), n_grid))
-    c_step_values = np.linspace(c_step_min, c_step_max, n_grid)
+    # --- init --------------------------------------------
+    b_min, b_max = range_b
+    c_min, c_max = range_c
+
+    b_values = np.linspace(b_min, b_max, n_grid)
+    c_values = c_sign * np.exp(np.linspace(np.log(c_min), np.log(c_max), n_grid))
 
     # --- grid search -------------------------------------
     cost_arr = np.zeros(shape=(n_grid, n_grid))
-    for i_e, c_exp in enumerate(c_exp_values):
-        for i_step, c_step in enumerate(c_step_values):
-            cost_arr[i_e, i_step] = fitting_cost(x, fx, c_step, c_exp, c_reg)
+    for i_b, b in enumerate(b_values):
+        for i_c, c in enumerate(c_values):
+            cost_arr[i_b, i_c] = fitting_cost(x, fx, a, b, c, reg)
 
     # --- return good enough results ----------------------
     cost_min = np.min(cost_arr)
     cost_threshold = tol_c0 + (tol_c1 * cost_min)
 
+    a_lst, b_lst, c_lst = [], [], []
+    cost_lst = []
+    for i_b, b in enumerate(b_values):
+        for i_c, c in enumerate(c_values):
+            cost = cost_arr[i_b, i_c]
+            if cost <= cost_threshold:
+                a_lst.append(a)
+                b_lst.append(b)
+                c_lst.append(c)
+                cost_lst.append(cost)
+
+    return np.array(a_lst), np.array(b_lst), np.array(c_lst), np.array(cost_lst)
+
+
+@numba.njit
+def fit_curve_random_walk(
+    x: np.ndarray,
+    fx: np.ndarray,
+    range_c_exp: tuple[float, float],
+    range_c_step: tuple[float, float],
+    exp_sign: float,
+    n_iters: int,
+    t_init: float,
+    t_final: float,
+    step_size_init: float,
+    step_size_final: float,
+    c_reg: float,
+    tol_c0: float,
+    tol_c1: float,
+    seed: int = 42,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Closely resembles 'fit_curve_brute_force' in terms of the problem it solves, the arguments and the return values.
+
+    The main difference is that this method does not use brute force 2d grid search, but rather uses a
+    Simulated Annealing style of approach.
+
+    Random steps of 'step_size' are taken and new evaluations are accepted with a probability that depends on the
+    function value & temperate.  Relative function deltas will always be used.  We will consider 2 deltas:
+      - delta wrt optimal cost
+      - delta wrt threshold cost (below which we return results).
+
+    Temperature is decreased exponentially from t_init to t_final; same for step size.  Step sizes are to be interpreted
+    wrt to a normalized 2d search space representing a unit box [0,1] x [0,1].
+
+    Reasonable tuning variables are probably...
+        n_iters         = 1000
+        t_init          = 1.0
+        t_final         = 0.1
+        step_size_init  = 0.1
+        step_size_final = 0.001
+
+    """
+
+    # --- initialize grid ---------------------------------
+    # MAP (z0, z1) in [0,1]x[0,1] -> [c_exp_min, c_exp_max]x[c_step_min,c_step_max]
+    # c_exp = np.exp2(c_exp_c0 + c_exp_c1 * z0)
+    c_exp_c0 = np.log2(range_c_exp[0])
+    c_exp_c1 = np.log2(range_c_exp[1] / range_c_exp[0])
+    # c_step = c_step_c0 + c_step_c1 * z1
+    c_step_c0 = range_c_step[0]
+    c_step_c1 = range_c_step[1] - range_c_step[0]
+
+    # this is the candidate that we will update each iteration
+    z = np.array([0.5, 0.5], dtype=np.float64)
+    z_cand = z
+
+    # --- initialize annealing ----------------------------
+    t = t_init
+    t_factor = (t_final / t_init) ** (1 / (n_iters - 1))
+    step_size = step_size_init
+    step_size_factor = (step_size_final / step_size_init) ** (1 / (n_iters - 1))
+
+    # --- initialize data structures ----------------------
+    cost_min = 1e3
+    cost_threshold = tol_c0 + (tol_c1 * cost_min)
+    cost_dict = Dict.empty(
+        key_type=types.UniTuple(types.float64, 2),
+        value_type=types.float64,
+    )  #  map (c_exp, c_step) -> cost
+
+    # --- simulated annealing -----------------------------
+    random.seed(seed)
+    for i in range(n_iters):
+        # generate new candidate
+        if i == 0:
+            # if i==0, we still need to evaluate the starting point z
+            z_cand = z
+        else:
+            # otherwise, we can start perturbing
+            z_cand[0] = np.clip(z[0] + (step_size * (1 - 2 * random.random())), 0.0, 1.0)
+            z_cand[1] = np.clip(z[1] + (step_size * (1 - 2 * random.random())), 0.0, 1.0)
+
+        # evaluate current z_cand
+        c_exp = np.exp2(c_exp_c0 + c_exp_c1 * z[0])
+        c_step = c_step_c0 + c_step_c1 * z[1]
+        cost = fitting_cost(x, fx, c_step, c_exp, c_reg)
+
+        # see if we should remember this one
+        if cost <= cost_threshold:
+            cost_dict[(c_exp, c_step)] = cost
+
+        # acceptance logic
+        p = 0.0
+        p += 0.5 * (1.0 if cost < cost_min else np.exp(-(cost - cost_min) / (t * cost_min)))
+        p += 0.5 * (1.0 if cost < cost_threshold else np.exp(-(cost - cost_threshold) / (t * cost_threshold)))
+        if random.random() <= p:
+            z = z_cand
+
+        # update cost_min
+        if cost < cost_min:
+            cost_min = cost
+            cost_threshold = tol_c0 + (tol_c1 * cost_min)
+
+        # update t, step_size
+        t *= t_factor
+        step_size *= step_size_factor
+
+    # --- return good enough results ----------------------
     c_exp_lst = []
     c_step_lst = []
     cost_lst = []
-    for i_e, c_exp in enumerate(c_exp_values):
-        for i_step, c_step in enumerate(c_step_values):
-            cost = cost_arr[i_e, i_step]
-            if cost <= cost_threshold:
-                c_exp_lst.append(c_exp)
-                c_step_lst.append(c_step)
-                cost_lst.append(cost)
+    for (c_exp, c_step), cost in cost_dict.items():
+        if cost <= cost_threshold:
+            c_exp_lst.append(c_exp)
+            c_step_lst.append(c_step)
+            cost_lst.append(cost)
 
     return np.array(c_exp_lst), np.array(c_step_lst), np.array(cost_lst)
 
@@ -76,18 +205,18 @@ def fit_curve_brute_force(
 #  Fitting curve & cost
 # =================================================================================================
 @numba.njit(inline="always")
-def fitting_cost(x: np.ndarray, fx: np.ndarray, c_step: float, c_exp: float, c_reg: float) -> float:
+def fitting_cost(x: np.ndarray, fx: np.ndarray, a: float, b: float, c: float, reg: float) -> float:
     """
     L1-cost of fitting fitting_curve to (x,fx)-values, where all x>0.
-    A regularization term is added that should help keep c_exp=1.0 and c_step=0.0 unless the data convincingly says
+    A regularization term is added that should help keep c=1.0 and b=0.0 unless the data convincingly says
     otherwise.
     """
-    fx_pred = fitting_curve(x, c_step, c_exp)
-    reg_term = c_reg * (abs(c_step) + abs(np.log10(c_exp)))
+    fx_pred = fitting_curve(x, a, b, c)
+    reg_term = reg * (abs(b) + abs(np.log10(c)))
     return float(np.mean(np.abs(fx - fx_pred)) + reg_term)
 
 
 @numba.njit(inline="always")
-def fitting_curve(x: np.ndarray, c_step: float, c_exp: float) -> np.ndarray:
-    """curve f(x) = c_step + (1-c_step)*(x**c_exp), assuming x>0"""
-    return c_step + (1 - c_step) * (np.pow(x, c_exp))
+def fitting_curve(x: np.ndarray, a: float, b: float, c: float) -> np.ndarray:
+    """curve g(x) = a * (b + (1-b)**(x^c), assuming x>0"""
+    return a * (b + (1 - b) * (np.pow(x, c)))
--- tests/analysis/roots/single_root/test_helpers_curve_fitting.py
+++ tests/analysis/roots/single_root/test_helpers_curve_fitting.py
@@ -9,64 +9,64 @@ from snuffled._core.utils.noise import noise_from_float
 #  fit_curve_brute_force
 # =================================================================================================
 @pytest.mark.parametrize("c_noise", [1e-9, 1e-6, 1e-4, 1e-2])
-@pytest.mark.parametrize("true_c_exp", [0.25, 0.5, 0.8, 1.0, 1.25, 2.0, 4.0])
-def test_fit_curve_brute_force_c_exp(true_c_exp: float, c_noise: float):
-    """Check if for simple cases we can reliably reverse-engineer c_exp"""
+@pytest.mark.parametrize("true_c", [0.25, 0.5, 0.8, 1.0, 1.25, 2.0, 4.0])
+def test_fit_curve_brute_force_c(true_c: float, c_noise: float):
+    """Check if for simple cases we can reliably reverse-engineer parameter 'c'"""
 
     # --- arrange -----------------------------------------
-    x_values = np.linspace(0.5, 1.5, 20)
-    fx_values = np.array([(x**true_c_exp) + (c_noise * noise_from_float(x)) for x in x_values])
+    x_values = np.linspace(0.5, 1.5, 21)
+    fx_values = np.array([(x**true_c) + (c_noise * noise_from_float(x)) for x in x_values])
 
     # --- act ---------------------------------------------
-    c_exp_values, c_step_values, cost_values = fit_curve_brute_force(
+    a_values, b_values, c_values, cost_values = fit_curve_brute_force(
         x=x_values,
         fx=fx_values,
-        range_c_exp=(0.1, 10),
-        range_c_step=(-0.5, 1.5),
-        exp_sign=1.0,
+        range_b=(-0.5, 1.0),
+        range_c=(0.1, 10),
+        c_sign=1.0,
         n_grid=201,
-        c_reg=1e-3,
+        reg=1e-3,
         tol_c0=0.0,
         tol_c1=2.0,
     )
 
-    print(f"c_exp  : {min(c_exp_values)} -> {max(c_exp_values)}")
-    print(f"c_step : {min(c_step_values)} -> {max(c_step_values)}")
+    print(f"b : {min(b_values)} -> {max(b_values)}")
+    print(f"c : {min(c_values)} -> {max(c_values)}")
 
     # --- assert ------------------------------------------
     # NOTE: we need to add some tolerances, since we search over a fixed grid
     #       that might not even contain the true value.
-    assert 0.99 * min(c_exp_values) <= true_c_exp <= 1.01 * max(c_exp_values)
-    assert min(c_step_values) - 0.01 <= 0.0 <= max(c_step_values) + 0.01
+    assert min(b_values) - 0.01 <= 0.0 <= max(b_values) + 0.01
+    assert 0.99 * min(c_values) <= true_c <= 1.01 * max(c_values)
 
 
 @pytest.mark.parametrize("c_noise", [1e-9, 1e-6, 1e-4, 1e-2])
-@pytest.mark.parametrize("true_c_step", [0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
-def test_fit_curve_brute_force_c_step(true_c_step: float, c_noise: float):
-    """Check if for simple cases we can reliably reverse-engineer c_step"""
+@pytest.mark.parametrize("true_b", [0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
+def test_fit_curve_brute_force_b(true_b: float, c_noise: float):
+    """Check if for simple cases we can reliably reverse-engineer parameter 'b'"""
 
     # --- arrange -----------------------------------------
-    x_values = np.linspace(0.5, 1.5, 20)
-    fx_values = np.array([true_c_step + (1 - true_c_step) * x + (c_noise * noise_from_float(x)) for x in x_values])
+    x_values = np.linspace(0.5, 1.5, 21)
+    fx_values = np.array([true_b + (1 - true_b) * x + (c_noise * noise_from_float(x)) for x in x_values])
 
     # --- act ---------------------------------------------
-    c_exp_values, c_step_values, cost_values = fit_curve_brute_force(
+    a_values, b_values, c_values, cost_values = fit_curve_brute_force(
         x=x_values,
         fx=fx_values,
-        range_c_exp=(0.1, 10),
-        range_c_step=(-0.5, 1.5),
-        exp_sign=1.0,
+        range_b=(-0.5, 1.0),
+        range_c=(0.1, 10),
+        c_sign=1.0,
         n_grid=201,
-        c_reg=1e-3,
+        reg=1e-3,
         tol_c0=0.0,
         tol_c1=2.0,
     )
 
-    print(f"c_exp  : {min(c_exp_values)} -> {max(c_exp_values)}")
-    print(f"c_step : {min(c_step_values)} -> {max(c_step_values)}")
+    print(f"b : {min(b_values)} -> {max(b_values)}")
+    print(f"c : {min(c_values)} -> {max(c_values)}")
 
     # --- assert ------------------------------------------
     # NOTE: we need to add some tolerances, since we search over a fixed grid
     #       that might not even contain the true value.
-    assert 0.99 * min(c_exp_values) <= 1.0 <= 1.01 * max(c_exp_values)
-    assert min(c_step_values) - 0.01 <= true_c_step <= max(c_step_values) + 0.01
+    assert min(b_values) - 0.01 <= true_b <= max(b_values) + 0.01
+    assert 0.99 * min(c_values) <= 1.0 <= 1.01 * max(c_values)
