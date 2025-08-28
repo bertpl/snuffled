8ec7b51e3d8e2e09519a10de5c1933708d784dce
Bert Pluymers <bert.pluymers@gmail.com>
2025-08-28 19:27:17 +0200

    Implement fit_curve_with_uncertainty_tailored, for lighting fast computation of optimally fitting curve + approximate parameter uncertainties.

--- tests/analysis/roots/single_root/curve_fitting/test_fit_tailored.py
+++ tests/analysis/roots/single_root/curve_fitting/test_fit_tailored.py
@@ -3,18 +3,63 @@ import math
 import numpy as np
 import pytest
 
-from snuffled._core.analysis.roots.single_root.curve_fitting import compute_x_deltas, fitting_curve
+from snuffled._core.analysis.roots.single_root.curve_fitting import compute_x_deltas, fitting_cost, fitting_curve
+from snuffled._core.analysis.roots.single_root.curve_fitting._curves_and_costs import compute_threshold_cost
 from snuffled._core.analysis.roots.single_root.curve_fitting._fit_tailored import (
     fit_curve_exact_three_points,
     fit_curve_tailored,
+    fit_curve_with_uncertainty_tailored,
     param_step,
 )
 from snuffled._core.utils.noise import noise_from_float
 
+
 # =================================================================================================
 #  TEST - Find solution WITH uncertainty
 # =================================================================================================
-pass
+@pytest.mark.parametrize("a_true", [1.0, 5.0])
+@pytest.mark.parametrize("b_true", [0.0, 0.2, 0.9])
+@pytest.mark.parametrize("c_true", [0.5, 1.0, 2.0])
+@pytest.mark.parametrize("c_noise", [0.0, 1e-9, 1e-6, 1e-3, 1.0])
+@pytest.mark.parametrize("rel_uncertainty_size", [0.5, 0.75, 1.0])
+def test_fit_curve_with_uncertainty_tailored(
+    a_true: float, b_true: float, c_true: float, c_noise: float, rel_uncertainty_size: float
+):
+    # --- arrange -----------------------------------------
+
+    # define optimization problem
+    x_values = compute_x_deltas(dx=0.5, k=5)
+    fx_values = fitting_curve(x_values, a_true, b_true, c_true)
+    for i, x in enumerate(x_values):
+        fx_values[i] += c_noise * noise_from_float(x)
+
+    reg = 1e-3
+    range_b = (-0.5, 1.0)
+    range_c = (0.25, 4.0)
+
+    # compute reference values to check correctness
+    fx_q25, fx_q50, fx_q75 = np.quantile(fx_values, [0.25, 0.50, 0.75])
+    a_opt, b_opt, c_opt = fit_curve_tailored(x_values, fx_values, range_b, range_c, reg)
+    cost_opt = fitting_cost(x_values, fx_values, a_opt, b_opt, c_opt, reg)
+    threshold_cost = compute_threshold_cost(rel_uncertainty_size, cost_opt, fx_q25, fx_q50, fx_q75)
+
+    # --- act ---------------------------------------------
+    a_values, b_values, c_values, cost_values = fit_curve_with_uncertainty_tailored(
+        x_values, fx_values, range_b, range_c, reg, rel_uncertainty_size=rel_uncertainty_size
+    )
+
+    # --- assert ------------------------------------------
+
+    # check if (a,b,c) vs cost are mutually correct
+    for a, b, c, cost in zip(a_values, b_values, c_values, cost_values):
+        expected_cost = fitting_cost(x_values, fx_values, a, b, c, reg)
+        assert cost == expected_cost
+
+    # check if cost_values are as expected
+    assert (
+        min(cost_values) <= cost_opt
+    )  # <= not ==, because we can serendipitously a better optimum during uncertainty exploration
+    assert 0.9 * threshold_cost <= max(cost_values) <= threshold_cost
 
 
 # =================================================================================================
