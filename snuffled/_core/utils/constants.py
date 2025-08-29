9b7bbc3213abd9e43ccc0499d3c0f9b774fd3f6b
Bert Pluymers <bert.pluymers@gmail.com>
2025-08-29 13:14:53 +0200

    Add various seed offsets.

--- snuffled/_core/utils/constants.py
+++ snuffled/_core/utils/constants.py
@@ -8,6 +8,15 @@ EPS = sys.float_info.epsilon  # 2**-52 for float64
 # we do not use the same seed in each, even when set
 # manually in the top-most function.
 
+# property extractors & co
+SEED_OFFSET_SNUFFLER = 3_061_002
+SEED_OFFSET_FUNCTION_SAMPLER = 3_541_253
+SEED_OFFSET_ROOTS_ANALYSER = 2_389_859
+SEED_OFFSET_SINGLE_ROOT_ANALYSER = 6_005_595
+
+# curve_fitting
+SEED_OFFSET_COMPUTE_X_DELTAS = 1_321_350
+
 # utils.sampling
 SEED_OFFSET_MULTI_SCALE_SAMPLES = 2_427_111
 SEED_OFFSET_SAMPLE_INTEGERS = 4_911_514
