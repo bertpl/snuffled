31e7de97bbaf32e44409bb4b7ee81dbad7851ae3
Bert Pluymers <bert.pluymers@gmail.com>
2025-09-03 16:41:27 +0200

    Allow also including negative counterpart of provided range_c in curve fitting procedure & uncertainty exploration.

--- snuffled/_core/analysis/roots/curve_fitting/specialized/_fit_optimal_with_uncertainty.py
+++ snuffled/_core/analysis/roots/curve_fitting/specialized/_fit_optimal_with_uncertainty.py
@@ -1,9 +1,10 @@
+import math
+
 import numpy as np
 
-from ..shared import compute_threshold_cost, fitting_cost
+from ..shared import compute_threshold_cost
 from ._explore_uncertainty import explore_uncertainty
 from ._fit_optimal import fit_curve
-from ._helpers import param_step
 
 
 # =================================================================================================
@@ -16,6 +17,7 @@ def fit_curve_with_uncertainty(
     range_b: tuple[float, float],
     range_c: tuple[float, float],
     reg: float,
+    include_negative_c: bool = False,
     n_iters: int = 15,
     uncertainty_size: float = 1.0,
     uncertainty_tol: float = 1e-3,
@@ -44,6 +46,7 @@ def fit_curve_with_uncertainty(
     :param range_b: (b_min, b_max)  range of b values
     :param range_c: (c_min, c_max)  range of c values  (>0)
     :param reg: (float) regularization coefficient that helps favour c=1.0, b=0.0  (e.g. 1e-3)
+    :param include_negative_c: (bool, default=False) if True, also check for fit with c-range (-c_max, -c_min)
     :param n_iters: (int, default=15) number of iterations (both for optimum finding as uncertainty exploration)
     :param uncertainty_size: (float, default=1.0) factor to influence size of uncertainty region; this parameter
                                    maps to the 'relative_margin' parameter of the compute_threshold_cost function
@@ -53,33 +56,64 @@ def fit_curve_with_uncertainty(
                i-th elements of these arrays should be interpreted as tuples (a[i], b[i], c[i]) having cost[i]
     """
 
-    # --- get optimal solution ----------------------------
-    a_opt, b_opt, c_opt, cost_opt = fit_curve(x, fx, range_a, range_b, range_c, reg, n_iters, debug_flag)
+    # --- init --------------------------------------------
+    if include_negative_c:
+        c_ranges = [range_c, (-range_c[1], -range_c[0])]
+    else:
+        c_ranges = [range_c]
+
+    # --- get optimal solutions ---------------------------
+    abc_opt_lst: list[tuple[float, float, float]] = []
+    cost_opt_lst: list[float] = []
+    for range_c in c_ranges:
+        # go over each range_c-value and find optimal fit
+        a_opt, b_opt, c_opt, cost_opt = fit_curve(x, fx, range_a, range_b, range_c, reg, n_iters, debug_flag)
+        abc_opt_lst.append((a_opt, b_opt, c_opt))
+        cost_opt_lst.append(cost_opt)
 
     # --- determine threshold cost for uncertainty --------
     fx_q25, fx_q50, fx_q75 = np.quantile(fx, [0.25, 0.5, 0.75])
     cost_threshold = compute_threshold_cost(
         relative_margin=uncertainty_size,
-        optimal_cost=cost_opt,
+        optimal_cost=min(cost_opt_lst),  # overall optimal cost
         fx_q25=fx_q25,
         fx_q50=fx_q50,
         fx_q75=fx_q75,
     )
 
     # --- get uncertainty bounds --------------------------
-    return explore_uncertainty(
-        x,
-        fx,
-        a_opt,
-        b_opt,
-        c_opt,
-        cost_opt,
-        cost_threshold,
-        range_a,
-        range_b,
-        range_c,
-        reg,
-        n_iters,
-        uncertainty_tol,
-        debug_flag,
+    a_lst: list[np.ndarray] = []
+    b_lst: list[np.ndarray] = []
+    c_lst: list[np.ndarray] = []
+    cost_lst: list[np.ndarray] = []
+    for (a_opt, b_opt, c_opt), cost_opt, range_c in zip(abc_opt_lst, cost_opt_lst, c_ranges):
+        if cost_opt <= cost_threshold:
+            # there is at least 1 solution in this c-range that falls within the cost_threshold
+            a_values, b_values, c_values, cost_values = explore_uncertainty(
+                x,
+                fx,
+                a_opt,
+                b_opt,
+                c_opt,
+                cost_opt,
+                cost_threshold,
+                range_a,
+                range_b,
+                range_c,
+                reg,
+                n_iters,
+                uncertainty_tol,
+                debug_flag,
+            )
+            a_lst.append(a_values)
+            b_lst.append(b_values)
+            c_lst.append(c_values)
+            cost_lst.append(cost_values)
+
+    # --- aggregate & return ------------------------------
+    return (
+        np.array([a for a_arr in a_lst for a in a_arr]),
+        np.array([b for b_arr in b_lst for b in b_arr]),
+        np.array([c for c_arr in c_lst for c in c_arr]),
+        np.array([cost for cost_arr in cost_lst for cost in cost_arr]),
     )
